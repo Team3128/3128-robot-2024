6eb71eb272e8089833ec1fd54a657fd95256622e
Mason-Lam <97353903+Mason-Lam@users.noreply.github.com>
2024-02-15 23:21:02 -0800

    Autos

--- src/main/java/frc/team3128/autonomous/AutoPrograms.java
+++ src/main/java/frc/team3128/autonomous/AutoPrograms.java
@@ -1,6 +1,7 @@
 package frc.team3128.autonomous;
 
 import edu.wpi.first.wpilibj2.command.Command;
+
 import common.utility.narwhaldashboard.NarwhalDashboard;
 
 /**
@@ -17,12 +18,30 @@ public class AutoPrograms {
     }
 
     private void initAutoSelector() {
-        final String[] autoStrings = new String[] {};
+        final String[] autoStrings = new String[] {
+            "lady-amp_3note",
+            "lady-amp_4note",
+            "lady-amp_5note",
+            "lady-middle_1note_leave",
+            "lady-middle_2note",
+            "lady-source_1note_leave",
+            "lady-source_3note",
+            "lady-source_leave",
+            "ram-amp_3note",
+            "ram-amp_4note",
+            "ram-middle_1note_leave",
+            "ram-middle_2note",
+            "ram-source_3note",
+            "ram-source_leave"
+        };
         NarwhalDashboard.getInstance().addAutos(autoStrings);
     }
 
     public Command getAutonomousCommand() {
         String selectedAutoName = NarwhalDashboard.getInstance().getSelectedAuto();
-        return null;
+        // String selectedAutoName = "Test";
+        final Command autoCommand = Trajectories.getPathPlannerAuto(selectedAutoName);
+
+        return autoCommand.beforeStarting(Trajectories.resetAuto());
     }
 }
--- src/main/java/frc/team3128/autonomous/Trajectories.java
+++ src/main/java/frc/team3128/autonomous/Trajectories.java
@@ -1,7 +1,41 @@
 package frc.team3128.autonomous;
 
-import java.util.HashMap;
-import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
+import common.core.commands.NAR_PIDCommand;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+
+import static edu.wpi.first.wpilibj2.command.Commands.parallel;
+import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
+import static edu.wpi.first.wpilibj2.command.Commands.sequence;
+import static edu.wpi.first.wpilibj2.command.Commands.waitSeconds;
+import static frc.team3128.Constants.AutoConstants.*;
+import static frc.team3128.Constants.FocalAimConstants.focalPointBlue;
+import static frc.team3128.Constants.FocalAimConstants.focalPointRed;
+import static frc.team3128.Constants.SwerveConstants.*;
+
+import frc.team3128.Robot;
+import frc.team3128.commands.CmdSwerveDrive;
+
+import static frc.team3128.commands.CmdManager.*;
+
+import java.util.function.DoubleSupplier;
+
+
+import frc.team3128.subsystems.Climber;
+import frc.team3128.subsystems.Intake;
+import frc.team3128.subsystems.Swerve;
 
 /**
  * Store trajectories for autonomous. Edit points here. 
@@ -9,37 +43,88 @@ import com.pathplanner.lib.path.PathPlannerPath;
  */
 public class Trajectories {
 
-    private static final HashMap<String, PathPlannerPath> trajectories = new HashMap<String, PathPlannerPath>();
+    private static final Swerve swerve = Swerve.getInstance();
+    private static final Intake intake = Intake.getInstance();
+    private static boolean turning = false;
 
     public static void initTrajectories() {
-        final String[] trajectoryNames = {};
 
-        for (final String trajectoryName : trajectoryNames) {
+        // TODO: add commands
+        NamedCommands.registerCommand("Intake", Intake.getInstance().intake(Intake.State.EXTENDED));
+        NamedCommands.registerCommand("Shoot", autoShoot());
+        NamedCommands.registerCommand("Amp", null);
+        NamedCommands.registerCommand("Drop", null);
 
-            if (trajectoryName.contains("mid")) {
-                trajectories.put(trajectoryName, PathPlannerPath.fromPathFile(trajectoryName));
-            } 
-            else {
-                trajectories.put(trajectoryName, PathPlannerPath.fromPathFile(trajectoryName));
-            }
-        }
-
-        // AutoBuilder.configureHolonomic(
-        //     swerve::getPose,
-        //     swerve::resetOdometry,
-        //     swerve::getRobotVelocity,
-        //     swerve::drive,
-        //     swerveKinematics,
-        //     CommandEventMap,
-        //     swerve
-        // );
+        AutoBuilder.configureHolonomic(
+            swerve::getPose,
+            swerve::resetOdometry,
+            swerve::getRobotVelocity,
+            Trajectories::drive,
+            new HolonomicPathFollowerConfig(
+                new PIDConstants(translationKP, translationKI, translationKD),
+                new PIDConstants(rotationKP, rotationKI, rotationKD),
+                maxAttainableSpeed,
+                trackWidth,
+                new ReplanningConfig(false, true)
+            ),
+            ()-> Robot.getAlliance() == Alliance.Red,
+            swerve
+        );
     }
 
-    // public static Command generateAuto(PathPlannerTrajectory trajectory) {
-    //     return AutoBuilder.fullAuto(trajectory);
-    // }
+    public static void drive(ChassisSpeeds velocity) {
+        if (!turning) swerve.drive(velocity);
+    }
 
-    // public static Command get(String name) {
-    //     return builder.fullAuto(trajectories.get(name));
-    // }
+    public static Command turnInPlace() {
+        DoubleSupplier setpoint = ()-> swerve.getTurnAngle(Robot.getAlliance() == Alliance.Red ? focalPointRed : focalPointBlue);
+        return new NAR_PIDCommand(
+            TURN_CONTROLLER, 
+            ()-> swerve.getYaw(), //measurement
+            setpoint, //setpoint
+            (double output) -> {
+                // final double x = RobotContainer.controller.getLeftX();
+                // final double y = RobotContainer.controller.getLeftY();
+                // Translation2d translation = new Translation2d(x,y).times(maxAttainableSpeed);
+                // if (Robot.getAlliance() == Alliance.Red || !swerve.fieldRelative) {
+                //     translation = translation.rotateBy(Rotation2d.fromDegrees(90));
+                // }
+                // else {
+                //     translation = translation.rotateBy(Rotation2d.fromDegrees(-90));
+                // }
+
+                Swerve.getInstance().drive(new Translation2d(), Units.degreesToRadians(output), true);
+            }
+        ).beforeStarting(runOnce(()-> CmdSwerveDrive.disableTurn()));
+    }
+ 
+    public static Command autoShoot() {
+        return sequence(
+            runOnce(()->{turning = true;}),
+            parallel(
+                rampUp(),
+                turnInPlace()
+                // runOnce(()-> CmdSwerveDrive.setTurnSetpoint(swerve.getTurnAngle(Robot.getAlliance() == Alliance.Red ? focalPointRed : focalPointBlue))),
+                // waitUntil(()-> CmdSwerveDrive.rController.atSetpoint())
+            ),
+            runOnce(()->{turning = false;}),
+            intake.outtakeNoRequirements(),
+            waitSeconds(1),
+            neutral(false)
+        );
+    }
+
+    public static Command getPathPlannerAuto(String name) {
+        return new PathPlannerAuto(name);
+    }
+
+    // TODO: make new
+    public static Command resetAuto() {
+        return sequence(
+            Intake.getInstance().reset(),
+            Climber.getInstance().reset()
+        );
+        
+    }
+    
 }
